3be6c5f131e3ef7c5744470ae27a770360d274d1
Roides Javier Cruz Lara <roidescruzlara@example.com>
2019-01-25 00:14:05 -0600

    add angular material framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { RepositoryDetailComponent } from './components/repository-detail/repository-detail.component';
+import { SearchService } from './services/search.service';
+
+import {
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatListModule,
+  MatDialogModule,
+  MAT_DIALOG_DEFAULT_OPTIONS
+} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    RepositoryDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatListModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [
+    SearchService,
+    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}
+  ],
+  entryComponents: [
+    RepositoryDetailComponent
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
